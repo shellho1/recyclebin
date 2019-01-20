dfff348eb299295479a35291c13f56900ede1b42
Weston Shellhorn <shellho1@msu.edu>
2019-01-20 01:56:09 -0500

    Reset to remove api key

---
+++ app/src/main/java/edu/msu/shellho1/spartahack2019/SplashActivity.java
@@ -0,0 +1,17 @@
+package edu.msu.shellho1.spartahack2019;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_splash);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
